ba7d6950153220db1c8778dd30a1e266b8fc4537
github-ci <you@example.com>
2025-04-22 16:38:45 +0000

    refactor(wechat-work-app-chat-bundle): 移除未使用的依赖并更新composer.json

--- src/WechatWorkAppChatBundle.php
+++ src/WechatWorkAppChatBundle.php
@@ -3,16 +3,9 @@
 namespace WechatWorkAppChatBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
-use Tourze\BundleDependency\BundleDependencyInterface;
 use Tourze\EasyAdmin\Attribute\Permission\AsPermission;
 
 #[AsPermission(title: '企业微信群聊服务')]
-class WechatWorkAppChatBundle extends Bundle implements BundleDependencyInterface
+class WechatWorkAppChatBundle extends Bundle
 {
-    public static function getBundleDependencies(): array
-    {
-        return [
-            \WechatWorkBundle\WechatWorkBundle::class => ['all' => true],
-        ];
-    }
 }
