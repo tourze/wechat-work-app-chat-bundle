6c2b6391553bf0231b2517d752af2a5e5729175a
github-ci <you@example.com>
2025-04-28 09:44:21 +0000

    fix: 修复字段错误

--- src/Entity/AppChat.php
+++ src/Entity/AppChat.php
@@ -26,7 +26,7 @@ class AppChat
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ORM\ManyToOne(targetEntity: Agent::class)]
     #[ORM\JoinColumn(nullable: false)]
--- src/Entity/BaseChatMessage.php
+++ src/Entity/BaseChatMessage.php
@@ -23,7 +23,7 @@ abstract class BaseChatMessage
     #[ORM\GeneratedValue(strategy: 'CUSTOM')]
     #[ORM\CustomIdGenerator(SnowflakeIdGenerator::class)]
     #[ORM\Column(type: Types::BIGINT, nullable: false, options: ['comment' => 'ID'])]
-    private ?string $id = '0';
+    private ?string $id = null;
 
     #[ORM\ManyToOne(targetEntity: AppChat::class)]
     #[ORM\JoinColumn(nullable: false)]
